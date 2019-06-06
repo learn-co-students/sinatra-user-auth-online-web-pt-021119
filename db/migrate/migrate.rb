85cc9abf93ba285c4370b9dc05fa409dc4f5e7d7
Carlton Dwayne <carltondgrandison@gmail.com>
2019-06-06 02:11:34 +0000

    Done.

---
+++ db/migrate/migrate.rb
@@ -0,0 +1,11 @@
+
+class User < ActiveRecord::Migration[5.0]
+
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,4 @@
 
 ActiveRecord::Schema.define(version: 0) do
 
-  def change
-    create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.string :password
-  end 
 end
